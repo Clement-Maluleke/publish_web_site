97d6108306ac08eec3f210e407b0cb2f60eb7b59
Clement Hlamulu Maluleke <154417296+Clement-Maluleke@users.noreply.github.com>
2025-02-07 21:47:44 +0200

    Add files via upload

---
+++ includes/head.php
@@ -0,0 +1,39 @@
+
+<style>
+   .icons{}
+.icons ul li{display:inline; float:right; }       
+.icons a img{  margin:4px;}
+.icons a:hover{ background-color:green;}
+
+</style>
+
+
+
+<div class="container">
+
+<div class="row">
+    <div class="col-lg-3">
+        <div class="logo">
+            <img src="logo3.png">
+        </div>
+    </div>
+
+
+
+    <div class="col-lg-9">
+     
+        <div class="icons"> 
+        
+            <ul>
+                <li> <a href=""> <img src="icons/call.png" alt=""> </a>  </li>
+                <li><a href=""> <img src="icons/email.JPG" alt=""> </a>  </li>
+                <li> <a href=""> <img src="icons/Github.png" alt=""> </a> </li>
+                <li> <a href=""> <img src="icons/LinkedI.png" alt=""> </a> </li>
+            </ul>
+            <input type="text" placeholder="Search..">
+ 
+        </div>
+
+    </div>
+
+</div>
---
+++ includes/navigation.php
@@ -0,0 +1,27 @@
+
+
+<div class="row">
+    <div class = "col-lg-12"> 
+        
+        <nav>
+            <ul>
+                <li> <a href="index.html">HOME</a></li>
+                <li> <a href="portfolio.html"> PORTFOLIO</a> </li>
+                <li> <a href="about_us.html"> ABOUT US </a> </li>
+                <li> <a href="download.html">  DOWNLOADS </a></li>
+                <li><a href="gallery.html">  GALLERY </a></li>
+        
+            </ul>
+
+
+        </nav>
+      
+    </div>
+
+    
+
+
+</div>
+
+
+</div>

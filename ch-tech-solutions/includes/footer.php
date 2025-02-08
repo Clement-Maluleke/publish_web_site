fbb18e309a4c6550632abae1002caa16baec4660
Clement Hlamulu Maluleke <154417296+Clement-Maluleke@users.noreply.github.com>
2025-02-08 07:47:31 +0200

    Add files via upload

---
+++ ch-tech-solutions/includes/footer.php
@@ -0,0 +1,21 @@
+   <!-- <style>
+   .foot{
+    background-color:red;
+    color:white;
+    padding:5%;
+  }
+   </style> -->
+    
+  <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="foot">
+                    <p>&copy; Copyrigt reserved | 2025 January </p>
+
+                </div>
+                
+            </div>
+        </div>
+
+    </div>
+   
